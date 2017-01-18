c0876c8b392db5d385ff496dcd7dff8ab964771f
ZachParris <zacharyjparris@gmail.com>
2017-01-17 22:33:22 -0600

    Setup DAL

---
+++ Bangazon/DAL/BangazonContext.cs
@@ -0,0 +1,14 @@
+﻿using Bangazon.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Bangazon.DAL
+{
+    public class BangazonContext
+    {
+        public virtual DbSet<Task> Task { get; set; }
+    }
+}
---
+++ Bangazon/DAL/BangazonRepo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bangazon.DAL
+{
+    public class BangazonRepo
+    {
+        private BangazonContext Context;
+
+        public BangazonRepo(BangazonContext _ctx)
+        {
+            Context = _ctx;
+        }
+        public BangazonRepo()
+        {
+            Context = new BangazonContext();
+        }
+
+    }
+}
